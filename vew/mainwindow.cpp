13f3c2afd23d47fe490ee297b3c3ceab0b071cbc
jpferrell <ferrell.jack20@gmail.com>
2024-03-04 16:22:25 -0500

    Added some menu bars along with close application action

--- vew/mainwindow.cpp
+++ vew/mainwindow.cpp
@@ -4,12 +4,42 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , m_menubar(new QMenuBar(parent))
+    , m_fileMenu(new QMenu("File"))
+    , m_editMenu(new QMenu("Edit"))
 {
     ui->setupUi(this);
+
+    _SetupMenuBars();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete m_fileMenu;
+    delete m_editMenu;
+    delete m_menubar;
+}
+
+void MainWindow::_SetupMenuBars()
+{
+    QAction *fileOpenAction = new QAction(tr("&Open"));
+    QAction *fileCloseAction = new QAction(tr("&Close"));
+    QAction *appExitAction = new QAction(tr("&Exit"));
+
+    QAction *preferenceAction = new QAction(tr("&Preferences"));
+
+    m_fileMenu->addAction(fileOpenAction);
+    m_fileMenu->addAction(fileCloseAction);
+    m_fileMenu->addAction(appExitAction);
+
+    m_editMenu->addAction(preferenceAction);
+
+    m_menubar->addMenu(m_fileMenu);
+    m_menubar->addMenu(m_editMenu);
+
+    this->setMenuBar(m_menubar);
+
+    connect(appExitAction, &QAction::triggered, this, &QApplication::quit);
 }
 
--- vew/mainwindow.h
+++ vew/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMenuBar>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,11 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    QMenuBar *m_menubar;
+    QMenu *m_fileMenu;
+    QMenu *m_editMenu;
+
+    void _SetupMenuBars();
 };
 #endif // MAINWINDOW_H
